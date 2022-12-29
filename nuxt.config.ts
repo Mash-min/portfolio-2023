63524666bff9c61392da9107b0532cb60aabde61
Mashiyyat <delossantos.mashiyyat@gmail.com>
2022-12-29 22:29:05 +0800

    chore: bootstrap implementation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+	css: ['~/node_modules/bootstrap/dist/css/bootstrap.min.css'],
+	plugins: [
+		{
+			src: '~/plugins/bootstrap.ts',
+			mode: 'client',
+		},
+	],
+});
